054a9c09f2580e653d6eca78b083000bdc9c61f7
tentenymy <meiyiyang1201@gmail.com>
2015-10-01 01:49:25 -0700

    change image name

--- 569_HW2/p1/p1_code/p1a.cpp
+++ 569_HW2/p1/p1_code/p1a.cpp
@@ -5,6 +5,8 @@ using namespace cv;
 using namespace std;
 
 
+
+
 class TextureClassifier
 {
 
@@ -15,26 +17,61 @@ class ImageOperater
 
 };
 
-
-void Test()
+char* StringChar(string str)
 {
-
+    int length = str.length();
+    char *ch = new char[length + 1];
+    strcpy(ch, str.c_str());
 }
+
+void File_Read (string filename, Image *ptr_image, int height, int width, int byteperpixel)
+{
+    FILE *file;
+    char *ch_filename = StringChar(filename);
+    if (!(file = fopen(ch_filename, "rb"))) {
+        cout << "Cannot open file: " << filename << endl;
+        exit(1);
+    }
+    size_t size = height * width * byteperpixel;
+    Image image[size];
+    fread(image, sizeof(Image), size, file);
+    fclose(file);
+    ptr_image = image;
+}
+
+
+
+/*void File_Write (string filename, int height, int width, int byteperpixel)
+{
+    char new_filename[60];
+    strcpy(new_filename, FOLDER_WRITE);
+    strcat(new_filename, filename_write);
+    if (!(file = fopen(new_filename, "wb")))
+    {
+        cout << "Cannot open file: " << filename_write <<endl;
+        exit(1);
+    }
+
+    Image image[height] [width] [byteperpixel];
+    memcpy(image, pt_image, sizeof(image));
+    fwrite(image, sizeof(Image), (size_t)width * height * byteperpixel, file);
+    fclose(file);
+}*/
 
 
 
 int main(int argc, char *argv[]){
 
     cout << "Hello World" << endl;
-    int i = 0;
-    while(argv[i])
-    {
-        cout << argv[i] << endl;
-        i++;
-    }
 
 
-    Test();
+
+    // Test
+    string filename = "p1/p1_image/p1_image_a/grass_01.raw";
+    Image *ptr_image = NULL;
+    File_Read(filename, ptr_image, 128, 128, 1);
+    cout << ptr_image[0] << endl;
+
 
 
 
